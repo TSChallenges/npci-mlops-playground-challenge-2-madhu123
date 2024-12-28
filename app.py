7651ed597f13d562fc8cd452098c266fd7b23799
madhu123 <madhubabu.garlapati@gmail.com>
2024-12-28 07:27:39 +0000

    updated app.py file

--- app.py
+++ app.py
@@ -2,80 +2,96 @@ import re
 import joblib
 import gradio
 import gradio as gr
+# Loading the Required Packages
+import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 # Load model
-trained_model = joblib.load(filename = "titanic-survival-pred-model.pkl")
+trained_model = joblib.load(filename = "random_forest_model.pkl")
 
 
-# Extracts the title (Mr, Ms, etc) from the name variable
-def get_title(passenger):
-    line = passenger
-    if re.search('Mrs', line):
-        return 'Mrs'
-    elif re.search('Mr', line):
-        return 'Mr'
-    elif re.search('Miss', line):
-        return 'Miss'
-    elif re.search('Master', line):
-        return 'Master'
-    else:
-        return 'Other'
+# Prediction function
+#model_rf = RandomForestRegressor(n_estimators=100, max_depth=10, random_state=42)
+# Fit the model
+#model_rf.fit(X_train_scaled, y_train.values.ravel())
 
+# Prediction for test set
+#y_pred = model_rf.predict(x_test_scaled)
+
+# Calculate the score/error
+#print("R2 score:", r2_score(y_test, y_pred))
+#print("Mean squared error:", mean_squared_error(y_test, y_pred))
 
 # UI - Input components
-in_Pid = gradio.Textbox(lines=1, placeholder=None, value="79", label='Passenger Id')
-in_Pclass = gradio.Radio([1, 2, 3], type="value", label='Passenger class')
-in_Pname = gradio.Textbox(lines=1, placeholder=None, value="Caldwell, Master. Alden Gates", label='Passenger Name')
-in_sex = gradio.Radio(["Male", "Female"], type="value", label='Gender')
-in_age = gradio.Textbox(lines=1, placeholder=None, value="14", label='Age of the passenger in yrs')
-in_sibsp = gradio.Textbox(lines=1, placeholder=None, value="0", label='No. of siblings/spouse of the passenger aboard')
-in_parch = gradio.Textbox(lines=1, placeholder=None, value="2", label='No. of parents/children of the passenger aboard')
-in_ticket = gradio.Textbox(lines=1, placeholder=None, value="248738", label='Ticket number')
-in_cabin = gradio.Radio(["Yes", "No"], type="value", label='Has Cabin')
-# in_cabin = gradio.Textbox(lines=1, placeholder=None, value="A5", label='Cabin number')
-in_embarked = gradio.Radio(["Southampton", "Cherbourg", "Queenstown"], type="value", label='Port of Embarkation')
-in_fare = gradio.Textbox(lines=1, placeholder=None, value="29", label='Passenger fare')
+in_tdedate = gradio.Textbox(lines=1, placeholder=None, value="2011-June-24", label='hourly date')
+in_season = gradio.Radio(["spring", "summer", "fall", "winter"], type="value", label='season')
+in_hr = gradio.Textbox(lines=1, placeholder=None, value="1am", label='hr')
+in_holiday = gradio.Radio(["Yes", "No"], type="value", label='is holiday')
+in_weekday = gradio.Radio(["Sat", "sun"], type="value", label=' weekday')
+in_workingday = gradio.Radio(["mon", "tue", "wed", "thu", "fri"], type="value", label='workingday')
+in_weather = gradio.Radio(["Heavy Rain", "Light Rain", "Mist", "Clear"], type="value", label='weatherist')
+in_temp = gradio.Textbox(lines=1, placeholder=None, value="10", label='temperature in celsius')
+in_atemp = gradio.Textbox(lines=1, placeholder=None, value="0", label='temperature in celsius')
+in_hum = gradio.Textbox(lines=1, placeholder=None, value="40", label='humidity')
+in_windspeed = gradio.Textbox(lines=1, placeholder=None, value="16.99", label='windspeed')
 
 # UI - Output component
 out_label = gradio.Textbox(type="text", label='Prediction', elem_id="out_textbox")
 
-
 # Mappings for categorical features
-embarked_mapping = {'Southampton': 0, 'Cherbourg': 1, 'Queenstown': 2}
-gender_mapping = {'Female': 0, 'Male': 1}
-title_mapping = {'Mrs': 4, 'Master':3 ,'Miss': 2, 'Mr': 1,'Other':0}
-cabin_mapping = {"Yes": 1, "No": 0}
+season_mapping = {'spring': 0, 'winter': 1, 'summer': 2, 'fall': 3}
+yr_mapping = {2011: 0, 2012: 1}
+mnth_mapping = {'January': 0, 'February': 1, 'December': 2, 'March': 3, 'November': 4, 'April': 5,
+                'October': 6, 'May': 7, 'September': 8, 'June': 9, 'July': 10, 'August': 11}
+weather_mapping = {'Heavy Rain': 0, 'Light Rain': 1, 'Mist': 2, 'Clear': 3}
+holiday_mapping = {'Yes': 0, 'No': 1}
+workingday_mapping = {'No': 0, 'Yes': 1}
+hour_mapping = {'4am': 0, '3am': 1, '5am': 2, '2am': 3, '1am': 4, '12am': 5, '6am': 6, '11pm': 7, '10pm': 8,
+                '10am': 9, '9pm': 10, '11am': 11, '7am': 12, '9am': 13, '8pm': 14, '2pm': 15, '1pm': 16,
+                '12pm': 17, '3pm': 18, '4pm': 19, '7pm': 20, '8am': 21, '6pm': 22, '5pm': 23}
+
 
 
 # Label prediction function
-def get_output_label(in_Pid, in_Pclass, in_Pname, in_sex, in_age, in_sibsp, in_parch, in_ticket, in_cabin, in_embarked, in_fare):
+def get_output_label(in_tdedate, in_season, in_hr, in_holiday, in_weekday, in_workingday, in_weather, in_temp, in_atemp, in_hum, in_windspeed):
     
-    input_df = pd.DataFrame({'Pclass': [in_Pclass], 
-                             'Sex': [gender_mapping[in_sex]], 
-                             'Age': [in_age],
-                             'Fare': [in_fare],
-                             'Embarked': [embarked_mapping[in_embarked]],
-                             'FamilySize': [int(in_sibsp) + int(in_parch) + 1], 
-                             'Has_cabin': [cabin_mapping[in_cabin]], 
-                             'Title': [title_mapping[get_title(in_Pname)]]})
+    input_df = pd.DataFrame({'tdedate': [in_tdedate], 
+                             'season': [season_mapping[in_season]], 
+                             'hour': [hour_mapping[in_hr]],
+                             'holiday': [holiday_mapping[in_holiday]],
+                             'weekday': [in_weekday],
+                             'workingday': [workingday_mapping[in_workingday]],
+                             #'yr': [yr_mapping[in_yr]],
+                             'weather': [weather_mapping[in_weather]],
+                             'temp': [in_temp],
+                             'atemp': [in_atemp],
+                             'hum': [in_hum],
+                             'windspeed': [in_windspeed]})
     
     prediction = trained_model.predict(input_df)     # Make a prediction using the saved model
-    if prediction[0] == 1:
-        label = "Likely to Survive"
-    else:
-        label = "Less likely to Survive"
+    print(prediction[0])
+#    if prediction[0] == 1:
+#        label = "Likely to Survive"
+#    else:
+#        label = "Less likely to Survive"
 
-    return label
+    return (prediction[0])
 
 
 # Create Gradio interface object
 iface = gradio.Interface(fn = get_output_label,
-                         inputs = [in_Pid, in_Pclass, in_Pname, in_sex, in_age, in_sibsp, in_parch, in_ticket, in_cabin, in_embarked, in_fare],
+                         inputs = [in_tdedate, in_season, in_hr, in_holiday, in_weekday, in_workingday, in_weather, in_temp, in_atemp, in_hum, in_windspeed],
                          outputs = [out_label],
-                         title="Titanic Survival Prediction API ⛴",
-                         description="Predictive model that answers the question: “What sort of people were more likely to survive?”",
+                         title="bike rental app",
+                         description="bike rent hours model",
                          flagging_mode='never'
                          )
 
